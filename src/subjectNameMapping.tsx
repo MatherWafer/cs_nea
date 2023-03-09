9075c98a04cf7dd248cf586d1116e01da8fcb3ca
Ayaan <ayaanmather@hotmail.com>
2023-03-09 12:35:23 +0000

    Milestone sync

---
+++ src/subjectNameMapping.tsx
@@ -0,0 +1,8 @@
+import GraphIntersect from "./students/questions/graphsIntersection"
+import KruskalsAlgorithm from "./students/questions/graphsKruskal"
+const SKILL_NAMES_AND_QUESTIONS = {
+    "KruskalAlgorithm": {questionComponent:KruskalsAlgorithm,
+                         name:"Kruskal's Algorithm"},
+    "GraphIntersections": {questionComponent:GraphIntersect,
+                         name:"Solving intersections of graphs"}
+}3
